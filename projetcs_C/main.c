3913a521dc0476608686af4594ec782c48521fbf
luan <luanbispo.sousa263@gmail.com>
2025-03-26 18:53:58 -0300

    Initial Commit

---
+++ projetcs_C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  projetcs_C
+//
+//  Created by luan on 26/03/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
